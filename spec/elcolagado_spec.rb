c5c0ccaf3e3b298af4018d48b138ec66ea2fc311
Jorge <kleer@kleer-csd-box>
2018-07-13 11:29:29 -0500

    Se agrego spec para captura letras

--- spec/elcolagado_spec.rb
+++ spec/elcolagado_spec.rb
@@ -6,4 +6,10 @@ describe ElColgado do
 		resultado = ec.ocultarPalabra("Ana")
 		expect(resultado).to eq "_ _ _"
 	end
+
+	it "debe de mostrar _ _ _" do
+		ec = ElColgado.new
+		resultado = ec.muestraCaracteresUsados("A")
+		expect(resultado).to eq "Caracteres usados: A"
+	end
 end
